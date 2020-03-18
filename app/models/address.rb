e30d7c03917e4f68aef7298519a77aa3bcfa02b1
asahiimura <58813834+asahiimura@users.noreply.github.com>
2020-03-19 05:23:58 +0900

    validates

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,10 @@
 class Address < ApplicationRecord
+  # validates :postal_code, presence: true, format: {with: /\A\d{3}[-]\d{4}$|^\d{3}[-]\d{2}$|^\d{3}$|^\d{5}$|^\d{7}\z/}
+  # validates :prefectures, presence: true
+  # validates :city_name, presence: true 
+  # validates :street_number, presence: true
+
+
+  
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, password_length: 7..128
+  
+  validates :nickname, presence: true
+  validates :last_name, presence: true, format: {with: /\A[一-龥ぁ-ん]/}
+  validates :first_name, presence: true, format: {with: /\A[一-龥ぁ-ん]/}
+  validates :last_furigana, presence: true, format: {with: /\A[ぁ-んー－]+\z/}
+  validates :first_furigana, presence: true, format: {with: /\A[ぁ-んー－]+\z/}
+  validates :birth_year, presence: true
+  validates :birth_month, presence: true
+  validates :birth_day, presence: true
+  validates :phone_number, format: {with: /\A\d{10,11}\z/}
+ 
+
 
 
   has_many :items
