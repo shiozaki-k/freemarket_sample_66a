fc4e968cba4d1fd9bd1dded1e4fb09b59e898c06
shiozaki-k <kzk330@gmail.com>
2020-03-28 13:31:36 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'items#index'
+  root to: "items#index"
   devise_for :users, controllers: { registrations: 'users/registrations' }
   get 'addresses', to: 'addresses#index'
 
