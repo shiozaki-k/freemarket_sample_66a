dbdd6fc1e6de8e16945f695dc21f2c10b8017e46
shiozaki-k <kzk330@gmail.com>
2020-03-29 18:45:00 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,11 @@ ActiveRecord::Schema.define(version: 2020_03_18_172133) do
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
