85211f0c7fbc88bc60822d411206f5328d20ee2d
333echo <3echo.law@gmail.com>
2020-04-03 21:28:37 +0900

    implementation of basic functions for selling 2nd
    implementation of basic functions for selling 2nd

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,7 +13,7 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to root_path
+      redirect_to root_path, notice: '出品完了しました'
     else
       render :new
     end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   get 'addresses', to: 'addresses#index'
 
-  root 'products#index'
   resources :products, except: :show
 
 end
