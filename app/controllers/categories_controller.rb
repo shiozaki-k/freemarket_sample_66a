cdf8fb445ac5d0569ad38b25d163914fa5728d2b
paisonthebaison <59052989+paisonthebaison@users.noreply.github.com>
2020-04-04 17:09:42 +0900

    ancestory parents fin

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,15 +4,17 @@ class CategoriesController < ApplicationController
     @parents = Category.where(ancestry: nil)
     render layout 'header'
   end
+
+  def new
+    @children = Category.find(params[:parent_id]).children
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
   # def index
   #   @parents = Category.all.order("id ASC").limit(13)
   # end
 
-  # def new
-  #   @children = Category.find(params[:parent_id]).children
-  #   respond_to do |format|
-  #     format.html
-  #     format.json
-  #   end
-  # end
+  
 end
