a8b3e716d53fead6985a4fe0b46fc7d563c59dd8
asahiimura <58813834+asahiimura@users.noreply.github.com>
2020-04-06 14:39:20 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: "items#index"
   devise_for :users, controllers: { registrations: 'users/registrations' }
-  resources :items, only: [:index,:show]
-
+  resources :items
 end
