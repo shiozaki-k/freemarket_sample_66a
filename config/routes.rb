32d796fc1ef122381fcac659356502a45f3f5127
shiozaki-k <kzk330@gmail.com>
2020-04-08 21:41:31 +0900

    mypage修正2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: "items#index"
   devise_for :users, controllers: { registrations: 'users/registrations' }
-  get 'user/edit' 
+  # get 'user/edit'
+  resource :users do
+    get 'edit'
+  end
   resource :posts do
     get 'logout'
     get 'card'
