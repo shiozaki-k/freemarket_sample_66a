2ec5f829e0756741f637d17990da605588a70537
paisonthebaison <59052989+paisonthebaison@users.noreply.github.com>
2020-04-12 11:23:15 +0900

    rest js

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,11 +1,9 @@
 $(function(){
-  $("li.header__inner__nav__left__category").hover(function(){
-    console.log(111)
-    // $(this).children().find("ul").remove()
-    console.log($(this).children().get(0))
-    // .find("ul").hide();  // 兄弟要素に含まれるサブメニューを全部消す。
-    $(this).children().slideDown(150);     // 自分のサブメニューを表示する。
- });
+//   $("li.header__inner__nav__left__category").hover(function(){
+//     $(this).children().find("ul").hide()
+//    // 兄弟要素に含まれるサブメニューを全部消す。
+//     $(this).children().slideDown(150);     // 自分のサブメニューを表示する。
+//  });
 //  $('html').click(function() {
 //     $('li.header__inner__nav__left__category li').slideUp(150);
 //  });
