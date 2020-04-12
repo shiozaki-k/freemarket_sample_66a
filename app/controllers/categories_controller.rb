ffbe4e8fe03599b2dc3958e0ff6541a37b3cb7f0
paisonthebaison <59052989+paisonthebaison@users.noreply.github.com>
2020-04-12 13:17:44 +0900

    before clone yamamura

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,17 +2,9 @@ class CategoriesController < ApplicationController
  
   def set_parents
     @parents = Category.where(ancestry: null)
-    binding.pry
     render layout 'header'
   end
 
-  # def new
-  #   @children = Category.find(params[:parent_id]).children
-  #   respond_to do |format|
-  #     format.html
-  #     format.json
-  #   end
-  # end
  
   
 end
