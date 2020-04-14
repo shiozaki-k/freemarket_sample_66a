3f87d7aedc5bd451f82f9d473661a9ed0d688b06
333echo <3echo.law@gmail.com>
2020-04-14 16:58:18 +0900

    test code_controller
    use RSpec and factory_bot

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe ProductsController, type: :controller do
+describe ProductsController, type: :controller do
+
+  describe 'GET #new' do
+    it "new.html.hamlに遷移すること" do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'GET #edit' do
+    it "edit.html.hamlに遷移すること" do
+    end
+  end
+
+  describe 'GET #index' do
+    it "@productsに正しい値が入っていること" do
+    end
+    it "index.html.hamlに遷移すること" do
+    end
+  end
 
 end
+
+
+# RSpec.describe ProductsController, type: :controller do
