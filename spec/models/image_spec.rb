2fbc1f41e09b0fee81980202e14f9979bc13425a
333echo <3echo.law@gmail.com>
2020-04-14 15:25:06 +0900

    test code_model
    use RSpec and factory_bot

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
+describe Image do
+  describe '#create' do
 
-RSpec.describe Image, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it "画像がない場合は登録できないこと" do
+      image = build(:image, src: nil)
+      image.valid?
+      expect(image.errors[:src]).to include("can't be blank")
+    end
+  end
 end
+
+# RSpec.describe Image, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,81 @@
 require 'rails_helper'
+describe Product do
+  describe '#create' do
 
-RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it "必須項目が存在すれば登録できること" do
+      product = build(:product)
+      expect(product).to be_valid
+    end
+
+    it "nameがない場合は登録できないこと" do
+      product = build(:product, name: "")
+      product.valid?
+      expect(product.errors[:name]).to include("can't be blank")
+    end
+    it "nameが41文字以上であれば登録できないこと" do
+      product = build(:product, name: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+      product.valid?
+      expect(product.errors[:name]).to include("is too long (maximum is 40 characters)")
+    end
+    it "nameが40文字以下であれば登録できること" do
+      product = build(:product, name: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+      expect(product).to be_valid
+    end
+
+    it "priceがない場合は登録できないこと" do
+      product = build(:product, price: "")
+      product.valid?
+      expect(product.errors[:price]).to include("can't be blank")
+    end
+
+    it "conditionがない場合は登録できないこと" do
+      product = build(:product, condition: "")
+      product.valid?
+      expect(product.errors[:condition]).to include("can't be blank")
+    end
+    it "conditionが1001文字以上であれば登録できないこと" do
+      product = build(:product, condition: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+      product.valid?
+      expect(product.errors[:condition]).to include("is too long (maximum is 1000 characters)")
+    end
+    it "nameが1000文字以下であれば登録できること" do
+      product = build(:product, condition: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+      expect(product).to be_valid
+    end
+
+    it "categoryがない場合は登録できないこと" do
+      product = build(:product, category: "")
+      product.valid?
+      expect(product.errors[:category]).to include("can't be blank")
+    end
+
+    it "descriptionがない場合は登録できないこと" do
+      product = build(:product, description: "")
+      product.valid?
+      expect(product.errors[:description]).to include("can't be blank")
+    end
+
+    it "shipping_feeがない場合は登録できないこと" do
+      product = build(:product, shipping_fee: "")
+      product.valid?
+      expect(product.errors[:shipping_fee]).to include("can't be blank")
+    end
+    it "shipping_originがない場合は登録できないこと" do
+      product = build(:product, shipping_origin: "")
+      product.valid?
+      expect(product.errors[:shipping_origin]).to include("can't be blank")
+    end
+
+    it "shipping_durationがない場合は登録できないこと" do
+      product = build(:product, shipping_duration: "")
+      product.valid?
+      expect(product.errors[:shipping_duration]).to include("can't be blank")
+    end
+
+  end
 end
+
+
+# RSpec.describe Product, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
