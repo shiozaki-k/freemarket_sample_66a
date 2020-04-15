9c989d4961194feb1824e0e6750db385e0fe4441
asahiimura <58813834+asahiimura@users.noreply.github.com>
2020-04-16 08:44:52 +0900

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,12 +6,12 @@ class ProductsController < ApplicationController
   end
 
   def new
-    if user_signed_in?
+    # if user_signed_in?
       @product = Product.new
       @product.images.new
-    else
-      redirect_to items_path
-    end
+    # else
+    #   redirect_to items_path
+    # end
   end
 
   def create
