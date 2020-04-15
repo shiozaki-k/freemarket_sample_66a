493c385c1920e50fd1007317106ffba6be1fa9ea
asahiimura <58813834+asahiimura@users.noreply.github.com>
2020-04-16 07:43:04 +0900

    テスト２

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :product do
-    
+    name                {"iphone8"}
+    price               {"5000"}
+    condition           {"新品・未使用"}
+    category            {"家電・スマホ・カメラ"}
+    brand               {"apple"}
+    description         {"最新式です"}
+    shipping_fee        {"送料込み（出品者負担）"}
+    shipping_origin     {"大阪"}
+    shipping_duration   {"１〜２日で発送"}
+    user_id             {1}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,63 +11,63 @@ describe User do
     it "is invalid without a nickname" do
       user = build(:user, nickname: nil)
       user.valid?
-      expect(user.errors[:nickname]).to include("can't be blank")
+      expect(user.errors[:nickname]).to include("を入力してください")
     end
 
     #  emailが空では登録できないこと
     it "is invalid without a email" do
       user = build(:user, email: nil)
       user.valid?
-      expect(user.errors[:email]).to include("can't be blank")
+      expect(user.errors[:email]).to include("を入力してください")
     end
 
     #  passwordが空では登録できないこと
     it "is invalid without a password" do
       user = build(:user, password: nil)
       user.valid?
-      expect(user.errors[:password]).to include("can't be blank")
+      expect(user.errors[:password]).to include("を入力してください")
     end
     #  last_nameが空では登録できないこと
     it "is invalid without a last_name" do
       user = build(:user, last_name: nil)
       user.valid?
-      expect(user.errors[:last_name]).to include("can't be blank")
+      expect(user.errors[:last_name]).to include("を入力してください")
     end
     #  first_nameが空では登録できないこと
     it "is invalid without a first_name" do
       user = build(:user, first_name: nil)
       user.valid?
-      expect(user.errors[:first_name]).to include("can't be blank")
+      expect(user.errors[:first_name]).to include("を入力してください")
     end
     #  last_furiganaが空では登録できないこと
     it "is invalid without a last_furigana" do
       user = build(:user, last_furigana: nil)
       user.valid?
-      expect(user.errors[:last_furigana]).to include("can't be blank")
+      expect(user.errors[:last_furigana]).to include("を入力してください")
     end
     #  first_furiganaが空では登録できないこと
     it "is invalid without a first_furigana" do
       user = build(:user, first_furigana: nil)
       user.valid?
-      expect(user.errors[:first_furigana]).to include("can't be blank")
+      expect(user.errors[:first_furigana]).to include("を入力してください")
     end
     #  birth_yearが空では登録できないこと
     it "is invalid without a birth_year" do
       user = build(:user, birth_year: nil)
       user.valid?
-      expect(user.errors[:birth_year]).to include("can't be blank")
+      expect(user.errors[:birth_year]).to include("を入力してください")
     end
     #  birth_monthが空では登録できないこと
     it "is invalid without a birth_month" do
       user = build(:user, birth_month: nil)
       user.valid?
-      expect(user.errors[:birth_month]).to include("can't be blank")
+      expect(user.errors[:birth_month]).to include("を入力してください")
     end
     #  birth_dayが空では登録できないこと
     it "is invalid without a birth_day" do
       user = build(:user, birth_day: nil)
       user.valid?
-      expect(user.errors[:birth_day]).to include("can't be blank")
+      expect(user.errors[:birth_day]).to include("を入力してください")
     end
 
 
@@ -82,7 +82,7 @@ describe User do
     it "is invalid without a password_confirmation although with a password" do
       user = build(:user, password_confirmation: "")
       user.valid?
-      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+      expect(user.errors[:password_confirmation]).to include("とPasswordの入力が一致しません")
     end
 
 
@@ -92,7 +92,7 @@ describe User do
     it "is invalid with a password that has less than 6 characters " do
       user = build(:user, password: "000000", password_confirmation: "000000")
       user.valid?
-      expect(user.errors[:password]).to include("is too short (minimum is 7 characters)")
+      expect(user.errors[:password]).to include("は7文字以上で入力してください")
     end
     #  passwordが7文字以上であれば登録できること
     it "is valid with a password that has more than 7 characters " do
@@ -106,7 +106,7 @@ describe User do
       user = create(:user)
       another_user = build(:user, email: user.email)
       another_user.valid?
-      expect(another_user.errors[:email]).to include("has already been taken")
+      expect(another_user.errors[:email]).to include("はすでに存在します")
     end
 
     #  重複したphone_numberが存在する場合登録できないこと
@@ -114,7 +114,7 @@ describe User do
       user = create(:user)
       another_user = build(:user, phone_number: user.phone_number)
       another_user.valid?
-      expect(another_user.errors[:phone_number]).to include("has already been taken")
+      expect(another_user.errors[:phone_number]).to include("はすでに存在します")
     end
 
  
@@ -132,49 +132,49 @@ describe User do
      it "is invalid without a last_name_ad" do
       user = build(:address, last_name_ad: nil)
       user.valid?
-      expect(user.errors[:last_name_ad]).to include("can't be blank")
+      expect(user.errors[:last_name_ad]).to include("を入力してください")
     end
      #  first_name_adが空では登録できないこと
      it "is invalid without a first_name_ad" do
       user = build(:address, first_name_ad: nil)
       user.valid?
-      expect(user.errors[:first_name_ad]).to include("can't be blank")
+      expect(user.errors[:first_name_ad]).to include("を入力してください")
     end
      #  last_furigana_adが空では登録できないこと
      it "is invalid without a last_furigana_ad" do
       user = build(:address, last_furigana_ad: nil)
       user.valid?
-      expect(user.errors[:last_furigana_ad]).to include("can't be blank")
+      expect(user.errors[:last_furigana_ad]).to include("を入力してください")
     end
      #  first_furigana_adが空では登録できないこと
      it "is invalid without a first_furigana_ad" do
       user = build(:address, first_furigana_ad: nil)
       user.valid?
-      expect(user.errors[:first_furigana_ad]).to include("can't be blank")
+      expect(user.errors[:first_furigana_ad]).to include("を入力してください")
     end
      #  postal_codeが空では登録できないこと
      it "is invalid without a postal_code" do
       user = build(:address, postal_code: nil)
       user.valid?
-      expect(user.errors[:postal_code]).to include("can't be blank")
+      expect(user.errors[:postal_code]).to include("を入力してください")
     end
      #  prefecturesが空では登録できないこと
      it "is invalid without a prefectures" do
       user = build(:address, prefectures: nil)
       user.valid?
-      expect(user.errors[:prefectures]).to include("can't be blank")
+      expect(user.errors[:prefectures]).to include("を入力してください")
     end
      #  city_nameが空では登録できないこと
      it "is invalid without a city_name" do
       user = build(:address, city_name: nil)
       user.valid?
-      expect(user.errors[:city_name]).to include("can't be blank")
+      expect(user.errors[:city_name]).to include("を入力してください")
     end
      #  street_numberが空では登録できないこと
      it "is invalid without a street_number" do
       user = build(:address, street_number: nil)
       user.valid?
-      expect(user.errors[:street_number]).to include("can't be blank")
+      expect(user.errors[:street_number]).to include("を入力してください")
     end
 
 
