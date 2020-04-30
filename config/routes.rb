4422a0defde5d3621b1e31a8b45c2f9e8ed00c55
paisonthebaison <59052989+paisonthebaison@users.noreply.github.com>
2020-04-30 21:37:53 +0900

    buy-by-creditcard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'buyers/index'
+  get 'buyers/done'
   get 'cards/new'
   get 'cards/show'
   root to: "items#index"
@@ -19,6 +21,12 @@ Rails.application.routes.draw do
     collection do
       get 'category_children' 
       get 'category_grandchildren'
+    end
+    resources :buyers, only: [:index] do
+      collection do
+        get 'done', to: 'buyers#done'
+        post 'pay', to: 'buyers#pay'
+      end
     end
   end
 
