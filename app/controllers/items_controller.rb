b0b0fdd4d90c887226aedf0fbc50744564ebad3a
shiozaki-k <kzk330@gmail.com>
2020-05-02 13:42:10 +0900

    カテゴリー別

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,25 +3,12 @@ class ItemsController < ApplicationController
 
   def index
     @products = Product.includes(:images).order('created_at DESC')
+    @parents = Category.where(ancestry: nil)
    
   end
 
   def show
-    
-
-    
+        
   end
-
-
-def index
-  @parents = Category.where(ancestry: nil)
-
-end
-
-
-def show
-  
-end
-
 
 end
