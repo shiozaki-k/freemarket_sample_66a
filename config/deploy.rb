fec7825e3fcfbcb6521adf279fe8f8639fdf5774
shiozaki-k <kzk330@gmail.com>
2020-05-06 20:42:35 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,12 +30,12 @@ after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
+    set :linked_files, fetch(:linked_files, []).push("config/master.key")
   end
   set :default_env, {
     rbenv_root: "/usr/local/rbenv",
     path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
     AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
     AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
-    set :linked_files, fetch(:linked_files, []).push("config/master.key")
   }
 end
