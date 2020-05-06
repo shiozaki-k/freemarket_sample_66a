b9799b4a0a8a11d4a763d8f9832d030852138615
shiozaki-k <kzk330@gmail.com>
2020-05-06 20:41:31 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,6 +36,6 @@ namespace :deploy do
     path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
     AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
     AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+    set :linked_files, fetch(:linked_files, []).push("config/master.key")
   }
-  set :linked_files, fetch(:linked_files, []).push("config/master.key")
 end
