029c40b3bd33465fceddd39fe0ee868d0f279088
shiozaki-k <kzk330@gmail.com>
2020-05-13 20:35:58 +0900

    tt

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,13 +1,25 @@
 class BookmarksController < ApplicationController
   def create
-    bookmark = current_user.bookmarks.build(board_id: params[:board_id])
-    bookmark.save!
-    redirect_to products_path, success: t('.flash.bookmark')
+    # bookmark = current_user.bookmarks.build(board_id: params[:board_id])
+    # bookmark.save!
+    # redirect_to products_path, success: t('.flash.bookmark')
+    @user_id = current_user.id
+    @category_id = Category.find(params[:id]).id
+    @bookmark = Bookmark.new(category_id: @category_id, user_id: @user_id)
+
+
+      if @boookmark.save
+        redirect_to user_path(current_user)
+      end
   end
 
   def destroy
-    current_user.bookmarks.find_by(board_id: params[:board_id]).destroy!
-    redirect_to products_path, success: t('.flash.not_bookmark')
+    # current_user.bookmarks.find_by(board_id: params[:board_id]).destroy!
+    # redirect_to products_path, success: t('.flash.not_bookmark')
+    @bookmark = Bookmark.find(params[:id])
+    if @clip.destroy
+      redirect_to user_path(current_user)
+    end
   end
 
   def show
--- db/migrate/20200507123328_create_bookmarks.rb
+++ db/migrate/20200507123328_create_bookmarks.rb
@@ -3,8 +3,9 @@ class CreateBookmarks < ActiveRecord::Migration[5.2]
     create_table :bookmarks do |t|
       t.references :user, foreign_key: true
       t.references :category, foreign_key: true
-
-      t.timestamps
+      t.timestamps null:false
     end
+    add_index :bookmarks, :user_id
+    add_index :bookmarks, :category_id
   end
 end
